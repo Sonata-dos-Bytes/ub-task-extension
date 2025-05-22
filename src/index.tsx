201d203ae379837a90095d210f5cd7a1fa96150c
Pedro Henrique Martins Borges <88720549+piedro404@users.noreply.github.com>
2025-05-22 23:41:26 +0000

    feat: refactor authentication context to use SessionProvider and add React SVG asset

--- src/index.tsx
+++ src/index.tsx
@@ -2,18 +2,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { HashRouter } from 'react-router-dom';
-import { AuthProvider } from './contexts/auth-context';
+import { SessionProvider } from './contexts/auth-context';
 import AppRoutes from './routes';
 import { Provider } from "./components/ui/provider"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider>
-      <AuthProvider>
+      <SessionProvider>
         <HashRouter>
           <AppRoutes />
         </HashRouter>
-      </AuthProvider>
+      </SessionProvider>
     </Provider>
   </StrictMode>,
 )
